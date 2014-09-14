1235c55c071757921d5b161a79ed5050e2c25664
Raffaello <raffaello.bonghi@minisumo.net>
2014-09-14 21:37:32 +0200

    Update Version for uNAV board

--- nbproject/Package-XC16_dsPIC33FJ128MC802.bash
+++ nbproject/Package-XC16_dsPIC33FJ128MC802.bash
@@ -10,9 +10,9 @@ CND_CONF=XC16_dsPIC33FJ128MC802
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/dsPIC33MotionControl.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=dsPIC33MotionControl.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=dspic33motioncontrol.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/uNAV.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=uNAV.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=unav.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/dspic33motioncontrol.x/bin
+makeDirectory ${TMPDIR}/unav.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/dspic33motioncontrol.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/unav.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/dspic33motioncontrol.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/unav.x.tar *
 checkReturnCode
 
 # Cleanup
