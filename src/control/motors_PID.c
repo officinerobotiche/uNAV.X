44aa534bea20cb074b4f1561cea0ae7f642d68a9
Walter Lucetti <myzhar@robot-home.it>
2015-01-22 00:19:09 +0100

    Fixed bug in setting Kd for motor left
    Kd was set with the same value of Kp!!!

--- src/control/motors_PID.c
+++ src/control/motors_PID.c
@@ -187,7 +187,7 @@ void init_pid_control(void) {
 void update_pid_l(void) {
     kCoeffs1[0] = Q15(pid_left.kp); //0.5
     kCoeffs1[1] = Q15(pid_left.ki); //0.6
-    kCoeffs1[2] = Q15(pid_left.kp); //0.0
+    kCoeffs1[2] = Q15(pid_left.kd); //0.0
     InitPid1(); //Init PIDL
 }
 
