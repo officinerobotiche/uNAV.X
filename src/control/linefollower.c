cc901c1c5ad93db291aa97e336d2bc2770dfa8c1
katodo <mauro.soligo@katodo.com>
2015-05-03 16:20:14 +0200

    First working code with simpler function to move 2 motors at a fixed speed.

--- src/control/linefollower.c
+++ src/control/linefollower.c
@@ -5,13 +5,23 @@
  * Created on 25 aprile 2015, 10.36
  */
 
+#include <stdint.h>        /* Includes uint16_t definition                    */
+#include <stdbool.h>       /* Includes true/false definition                  */
+
 #include "control/high_level_control.h"
+#include "control/motors_PID.h"
 
 extern velocity_t vel_rif, vel_mis;
+extern unsigned int counter_alive[NUM_MOTORS];
 
 void linefollowing()
 {
-    // Our world!!!!
+    //Reset time emergency
+    counter_alive[0] = 0;
+    counter_alive[1] = 0;
+               
+    // Our Hello World!!!! :)
+ 
     vel_rif.v = 0.2;
     
 }
