ac0dbdf8ae1a09a29a6eeeb078103fa2fe5b634a
katodo <mauro.soligo@katodo.com>
2015-05-05 00:39:47 +0200

    Created base function for Infrared Reading, now they are in a very primordial state :)

--- includes/control/linefollower.h
+++ includes/control/linefollower.h
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2014 Officine Robotiche
- * Author: Raffaello Bonghi
- * email:  raffaello.bonghi@officinerobotiche.it
+ * Copyright (C) 2015 Officine Robotiche
+ * Author: Mauro Soligo
+ * email:  mauro.soligo@officinerobotiche.it
  * Permission is granted to copy, distribute, and/or modify this program
  * under the terms of the GNU Lesser General Public License, version 2 or any
  * later version published by the Free Software Foundation.
@@ -15,30 +15,52 @@
  * Public License for more details
  */
 
+
 #ifndef LINEFOLLOWER_H
 #define	LINEFOLLOWER_H
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
 
     /**************************************************************************/
     /* System Level #define Macros                                            */
     /**************************************************************************/
+    #define INPUT       1
+    #define OUTPUT      0
 
+    #define PORT_IO0    _TRISA9
+    #define PORT_IO1    _TRISC0
+    #define PORT_IO2    _TRISC1
+    #define PORT_IO3    _TRISC2
+    #define PORT_IO4    _TRISC3
+    #define PORT_IO5    _TRISA4
+    #define PORT_IO6    _TRISB4
 
-    /******************************************************************************/
-    /* System Function Prototypes                                                 */
-    /******************************************************************************/
+    #define PIN_IO0     PORTAbits.RA9
+    #define PIN_IO1     PORTCbits.RC0       
+    #define PIN_IO2     PORTCbits.RC1
+    #define PIN_IO3     PORTCbits.RC2
+    #define PIN_IO4     PORTCbits.RC3
+    #define PIN_IO5     PORTAbits.RA4
+    #define PIN_IO6     PORTBbits.RB4
+    
+
+    /**************************************************************************/
+    /* LineFollower extern variables                                          */
+    /**************************************************************************/
+
+    
+    /**************************************************************************/
+    /* System Function Prototypes                                             */
+    /**************************************************************************/
 
     /**
      * main function of line following algoritms.
      */
     void linefollowing(void);
 
+    /*
+     * IR senso reading related functions
+     */
+    void IRsensor_CapacitorDisCharge(void);    
+    void IRsensor_StartMeasure(void);
 
-#ifdef	__cplusplus
-}
-#endif
 
-#endif	/* MOTORSPID_H */
+#endif	/* LINEFOLLOWER_H */
--- src/control/linefollower.c
+++ src/control/linefollower.c
@@ -5,14 +5,28 @@
  * Created on 25 aprile 2015, 10.36
  */
 
+/* Device header file */
+#if defined(__XC16__)
+#include <xc.h>
+#elif defined(__C30__)
+#if defined(__dsPIC33E__)
+#include <p33Exxxx.h>
+#elif defined(__dsPIC33F__)
+#include <p33Fxxxx.h>
+#endif
+#endif
+
 #include <stdint.h>        /* Includes uint16_t definition                    */
 #include <stdbool.h>       /* Includes true/false definition                  */
 
 #include "control/high_level_control.h"
 #include "control/motors_PID.h"
+#include "control/linefollower.h"
 
 extern velocity_t vel_rif, vel_mis;
 extern unsigned int counter_alive[NUM_MOTORS];
+
+int16_t pippoooo;
 
 void linefollowing()
 {
@@ -22,6 +36,69 @@ void linefollowing()
                
     // Our Hello World!!!! :)
  
-    vel_rif.v = 0.2;
+    vel_rif.v = 0.0;
     
+    /*
+     *  test of sequential calling of IRsensor_XXXXX function
+     *  to try a simple measure to evaluate sensor performance with 
+     *  Oscilloscope
+     */
+    if(pippoooo == 0)
+    {   pippoooo = 1;
+        IRsensor_CapacitorDisCharge();
+        
+    }
+    else
+    {   
+        if(pippoooo ==1 )
+        {   pippoooo++;
+            IRsensor_StartMeasure(); 
+        }
+        else
+        {
+            if( pippoooo < 50 )
+                pippoooo++;
+            else
+                pippoooo = 0;
+        }
+    }
+}
+
+
+void IRsensor_CapacitorDisCharge(void)
+{   
+    /*
+     * Set IO0 ... IO6 pin as output
+     */
+    PORT_IO0 = OUTPUT; // Set IO0 as Output
+    PORT_IO1 = OUTPUT; // Set IO1 as Output
+    PORT_IO2 = OUTPUT; // Set IO2 as Output
+    PORT_IO3 = OUTPUT; // Set IO3 as Output
+    PORT_IO4 = OUTPUT; // Set IO4 as Output
+    PORT_IO5 = OUTPUT; // Set IO5 as Output
+    PORT_IO6 = OUTPUT; // Set IO6 as Output
+    
+    /* 
+     * Setting pin to HighLevel discharge capacitor used in infrared sensor
+     */ 
+    PIN_IO0 = true;
+    PIN_IO1 = true;
+    PIN_IO2 = true;
+    PIN_IO3 = true;
+    PIN_IO4 = true;
+    PIN_IO5 = true;
+    PIN_IO6 = true;
+    
+}
+
+void IRsensor_StartMeasure(void)
+{
+    // Set IO0 ... IO6 pin as input
+    PORT_IO0 = INPUT; // Set IO0 as Input
+    PORT_IO1 = INPUT; // Set IO1 as Input
+    PORT_IO2 = INPUT; // Set IO2 as Input
+    PORT_IO3 = INPUT; // Set IO3 as Input
+    PORT_IO4 = INPUT; // Set IO4 as Input
+    PORT_IO5 = INPUT; // Set IO5 as Input
+    PORT_IO6 = INPUT; // Set IO6 as Input
 }
