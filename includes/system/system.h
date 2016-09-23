996063323dfb503345247367903be5d8d754c87b
Raffaello Bonghi <raffaello@rnext.it>
2016-09-23 22:36:00 +0200

    Set at 1000 Hz Task Manager

--- includes/system/system.h
+++ includes/system/system.h
@@ -32,7 +32,7 @@ extern "C" {
 #define SYS_FREQ        80000000
 #define FCY             SYS_FREQ/2
 
-#define FRTMR1 10000            // Timer1 - Value in herz [Hz]
+#define FRTMR1 1000            // Timer1 - Value in herz [Hz]
 #define TCTMR1 1/FRTMR1         // Timer1 - Value in seconds [s]
 #define TMR1_VALUE FCY/FRTMR1   // Timer1 - Value in CLK
 #define FRTMR2 SYS_FREQ
