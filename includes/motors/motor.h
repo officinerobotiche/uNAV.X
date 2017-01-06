8bd684172c8ea78111c9eecb98c9204acb1633f2
Raffaello Bonghi <raffaello@rnext.it>
2017-01-06 17:27:55 +0100

    New Motor initialization

--- includes/motors/motor.h
+++ includes/motors/motor.h
@@ -21,11 +21,11 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
-    
+
 /******************************************************************************/
 /*	Include																	  */
 /******************************************************************************/
-    
+
 #include <stdint.h>        /* Includes uint16_t definition                    */
 #include <stdbool.h>       /* Includes true/false definition                  */
 #include <dsp.h>
@@ -36,103 +36,127 @@ extern "C" {
 #include <or_peripherals/GPIO/adc.h>
 
 #include <or_math/statistics.h>
-    
+
 /******************************************************************************/
 /* Prototype              													  */
 /******************************************************************************/
-    
+
 #define NUM_CONTROLLERS 3
-// Get the number in controller array from enum_state_t
+    // Get the number in controller array from enum_state_t
 #define GET_CONTROLLER_NUM(X) ((X) - 1)
-    
-typedef struct _analog {
-    int32_t gain;
-    int32_t offset;
-} analog_t;
 
-/**
- * Definition of PID ecosystem
- */
-typedef struct _pid_control {
-    // Message information about value of PID
-    motor_pid_t pid;
-    // Struct of PID from dsp library
-    tPID PIDstruct;
-    // Coefficients KP, KI, KD
-    fractional kCoeffs[3];
-    // Coefficient K anti wind up
-    fractional k_aw;
-    // soft timer to launch PID controller
-    soft_timer_t timer;
-    // enable
-    volatile bool enable;
-    // anti wind up correction
-    volatile fractional saturation;
-} pid_controller_t;
-    
-typedef struct _motor_firmware {
-    unsigned int index;
-    // Information for Input Capture
-//    ICdata* ICinfo;
-    // Enable pin for H-bridge
-    gpio_t* pin_enable;
-    gpio_adc_t *adc;
-//    event_prescaler_t prescaler_callback;
-    // Frequency manager;
-    frequency_t manager_freq;
-    volatile int pwm_limit;
-    /// Task register
-    hEvent_t motor_manager_event;
-    hTask_t task_manager;
-    hTask_t task_emergency;
-    /// Motor position
-    uint32_t angle_limit;
-    volatile int PulsEnc;
-    volatile int32_t enc_angle;
-    //Emergency
-    struct {
-        soft_timer_t alive;
-        soft_timer_t stop;
-        uint16_t step;
-    } motor_emergency;
-    motor_emergency_t emergency;
-    bool save_velocity;
-    motor_control_t last_reference;
-    // Safety
-    struct {
-        hTask_t task_safety;
-        soft_timer_t stop;
-        soft_timer_t restore;
-        motor_state_t old_state;
-        uint16_t step;
-    } motor_safety;
-    motor_safety_t safety;
-    // Parameter and diagnostic
-    motor_diagnostic_t diagnostic;
-    motor_parameter_t parameter_motor;
-    //gain motor
-    int32_t k_vel_ic, k_vel_qei;
-    float k_ang;
-    // Velocity mean
-    statistic_buffer mean_vel;
-    //Internal value volt and current
-    analog_t volt, current;
-    //PID
-    motor_state_t state;
-    fractional pwm_saturation;
-    volatile motor_control_t external_reference;
-    volatile motor_control_t control_output;
-    pid_controller_t controller[NUM_CONTROLLERS];
-    //Common
-    motor_t constraint;
-    volatile motor_t controlOut;
-    volatile motor_t reference;
-    volatile motor_t measure;
-} motor_firmware_t;
+    typedef enum {
+        CONTROL_SAFETY = -2, ///< Motor disabled for high current
+        CONTROL_EMERGENCY = -1, ///< Motor slow down to zero speed, then the bridge is turned off
+        CONTROL_DISABLE = 0, ///< Motor disabled
+        CONTROL_POSITION = 1, ///< Motor controlled in position
+        CONTROL_VELOCITY = 2, ///< Motor controlled in velocity
+        CONTROL_CURRENT = 3, ///< Motor controller in torque
+        CONTROL_DIRECT = 4, ///< Motor controlled using direct PWM signals
+    } enum_state_t;
 
-/******************************************************************************/
-/* System Function Prototypes                                                 */
-/******************************************************************************/
+    typedef struct _ICdata {
+        volatile unsigned int delta;
+        volatile unsigned int k_mul; // k_vel multiplier according to IC scale
+        volatile unsigned int overTmr; //Overflow timer
+        volatile unsigned int oldTime; //Old time stored
+        volatile unsigned long timePeriod; //Time period from Input Capture
+        volatile int SIG_VEL; //Sign of versus rotation motor
+        volatile unsigned short number; //Mode of Input Capture
+    } ICdata;
+    
+    typedef void (*event_prescaler_t)(int motIdx);
+
+    typedef struct _analog {
+        int32_t gain;
+        int32_t offset;
+    } analog_t;
+
+    /**
+     * Definition of PID ecosystem
+     */
+    typedef struct _pid_control {
+        // Message information about value of PID
+        motor_pid_t pid;
+        // Struct of PID from dsp library
+        tPID PIDstruct;
+        // Coefficients KP, KI, KD
+        fractional kCoeffs[3];
+        // Coefficient K anti wind up
+        fractional k_aw;
+        // soft timer to launch PID controller
+        soft_timer_t timer;
+        // enable
+        volatile bool enable;
+        // anti wind up correction
+        volatile fractional saturation;
+    } pid_controller_t;
+
+    typedef struct _motor_firmware {
+        unsigned int index;
+        // Information for Input Capture
+        ICdata* ICinfo;
+        // Enable pin for H-bridge
+        gpio_t* pin_enable;
+        gpio_adc_t *adc;
+        event_prescaler_t prescaler_callback;
+        // Frequency manager;
+        frequency_t manager_freq;
+        volatile int pwm_limit;
+        /// Task register
+        hEvent_t motor_manager_event;
+        hTask_t task_manager;
+        hTask_t task_emergency;
+        /// Motor position
+        uint32_t angle_limit;
+        volatile int PulsEnc;
+        volatile int32_t enc_angle;
+        //Emergency
+
+        struct {
+            soft_timer_t alive;
+            soft_timer_t stop;
+            uint16_t step;
+        } motor_emergency;
+        motor_emergency_t emergency;
+        bool save_velocity;
+        motor_control_t last_reference;
+        // Safety
+
+        struct {
+            hTask_t task_safety;
+            soft_timer_t stop;
+            soft_timer_t restore;
+            motor_state_t old_state;
+            uint16_t step;
+        } motor_safety;
+        motor_safety_t safety;
+        // Parameter and diagnostic
+        motor_diagnostic_t diagnostic;
+        motor_parameter_t parameter_motor;
+        //gain motor
+        int32_t k_vel_ic, k_vel_qei;
+        float k_ang;
+        // Velocity mean
+        statistic_buffer mean_vel;
+        //Internal value volt and current
+        analog_t volt, current;
+        //PID
+        motor_state_t state;
+        fractional pwm_saturation;
+        volatile motor_control_t external_reference;
+        volatile motor_control_t control_output;
+        pid_controller_t controller[NUM_CONTROLLERS];
+        //Common
+        motor_t constraint;
+        volatile motor_t controlOut;
+        volatile motor_t reference;
+        volatile motor_t measure;
+    } motor_firmware_t;
+
+    /******************************************************************************/
+    /* System Function Prototypes                                                 */
+    /******************************************************************************/
 
 #ifdef	__cplusplus
 }
--- src/motors/motor.c
+++ src/motors/motor.c
@@ -21,6 +21,12 @@
 
 #include "motors/motor.h"
 
+#define MOTOR_DEFAULT_ADC_PORTS 2
+
+#define DEFAULT_FREQ_MOTOR_MANAGER 1000.0             // Task Manager 10Khz
+#define DEFAULT_FREQ_MOTOR_CONTROL_EMERGENCY 1000.0   // In Herts
+#define DEFAULT_FREQ_MOTOR_CONTROL_SAFETY 1000.0      // In Herts
+
 /******************************************************************************/
 /* Global Variable Declaration                                                */
 /******************************************************************************/
@@ -28,3 +34,120 @@
 /******************************************************************************/
 /* Communication Functions                                                    */
 /******************************************************************************/
+
+void Motor_TaskController(int argc, int *argv) {
+    
+}
+
+void Motor_Emergency(int argc, int *argv) {
+    
+}
+
+void Motor_Safety(int argc, int *argv) {
+    
+}
+
+void Motor_ADC_callback(void* obj) {
+    
+}
+
+/**
+ * @brief Reset Motor values
+ * @param motor The motor controller structure
+ */
+void Motor_reset_data(volatile motor_t* motor) {
+    motor->position_delta = 0;
+    motor->position = 0;
+    motor->velocity = 0;
+    motor->current = 0;
+    motor->effort = 0;
+    motor->pwm = 0;
+}
+/**
+ * @brief Initialization PID controllers
+ * @param motor The motor controller structure
+ * @param abcCoefficient ABC coefficient for PID
+ * @param controlHistory History structure for PID
+ * @param i The number of controller
+ */
+void Motor_initialize_controllers(motor_firmware_t *motor, 
+        fractional *abcCoefficient, fractional *controlHistory, unsigned int i) {
+    //Initialize the PID data structure: PIDstruct
+    //Set up pointer to derived coefficients
+    motor->controller[i].PIDstruct.abcCoefficients = abcCoefficient;
+    //Set up pointer to controller history samples
+    motor->controller[i].PIDstruct.controlHistory = controlHistory;
+    // Clear the controller history and the controller output
+    PIDInit(&motor->controller[i].PIDstruct);
+    // Enable
+    motor->controller[i].enable = false;
+    // Saturation value
+    motor->controller[i].saturation = 0;
+    // Gain anti wind up
+    motor->controller[i].k_aw = 0;
+}
+
+void Motor_init(motor_firmware_t *motor, unsigned int index, 
+        fractional *abcCoefficient, fractional *controlHistory, 
+        ICdata* ICinfo, event_prescaler_t prescaler_event, unsigned int PWM_LIMIT) {
+    unsigned int i;
+    // Set index number
+    motor->index = index;
+    // reset motor values
+    Motor_reset_data(&motor->measure);
+    Motor_reset_data(&motor->reference);
+    Motor_reset_data(&motor->controlOut);
+    // PWM limit
+    motor->pwm_limit = PWM_LIMIT;
+    // Initialize all controllers
+    for(i = 0; i < NUM_CONTROLLERS; i++) {
+        Motor_initialize_controllers(motor, &abcCoefficient[i], &controlHistory[i], i);
+    }
+    // Register input capture
+    motor->ICinfo = ICinfo;
+    motor->prescaler_callback = prescaler_event;
+    // Set null ADC ports
+    motor->adc = NULL;
+    // Set null enable ports
+    motor->pin_enable = NULL;
+    // reset values
+    motor->control_output = 0;
+    motor->external_reference = 0;
+    motor->pwm_saturation = 0;
+    //Setup diagnostic
+    motor->diagnostic.time_control = 0;
+    motor->diagnostic.watt = 0;
+    motor->diagnostic.temperature = 0;
+    motor->diagnostic.time_control = 0;
+    motor->diagnostic.state = CONTROL_DISABLE;
+    // Initialize mean buffer
+    init_statistic_buffer(&motor->mean_vel);
+    
+    // Setup frequency task manager
+    motor->manager_freq = DEFAULT_FREQ_MOTOR_MANAGER;
+    /// Register event and add in task controller - Working at 1KHz
+    motor->motor_manager_event = register_event_p(&Motor_TaskController, EVENT_PRIORITY_MEDIUM);
+    motor->task_manager = task_load_data(motor->motor_manager_event, motor->manager_freq, 1, motor);
+    /// Load controller EMERGENCY - Working at 1KHz
+    hEvent_t emergency_event = register_event_p(&Motor_Emergency, EVENT_PRIORITY_HIGH);
+    motor->task_emergency = task_load_data(emergency_event, DEFAULT_FREQ_MOTOR_CONTROL_EMERGENCY, 1, motor);
+    /// Load controller SAFETY - Working at 1KHz
+    hEvent_t safety_event = register_event_p(&Motor_Safety, EVENT_PRIORITY_HIGH);
+    motor->motor_safety.task_safety = task_load_data(safety_event, DEFAULT_FREQ_MOTOR_CONTROL_SAFETY, 1, motor);
+}
+
+void Motor_register_adc(motor_firmware_t *motor, gpio_adc_t *adc) {
+    /// Setup ADC current and temperature
+    motor->adc = adc;
+    gpio_adc_register(adc, MOTOR_DEFAULT_ADC_PORTS, &Motor_ADC_callback, motor);
+}
+
+void Motor_register_enable(motor_firmware_t *motor, const gpio_t* enable) {
+    /// Setup bit enable
+    motor->pin_enable = (gpio_t*) enable;
+    gpio_init_pin(motor->pin_enable);
+}
+
+void Motor_run(motor_firmware_t *motor, task_status_t state) {
+    task_set(motor->task_manager, state);
+}
